b928f6cf57a050b30c008752be10fb4f2fd5a7c3
schmidgallm <schmidgallm@outlook.com>
2020-06-29 20:39:43 -0700

    init commit || basic file folder structure

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// Dependencies
+const http = require('http');
+const app = require('../app');
+
+// Init Port and create server and add listener for server start
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`> Server now listening on port ${PORT}`);
+});
